8c0896bc0b5f6c6ee4fd0e67e442461e1233f904
zhengwenbo <19802021891@139.com>
2023-03-07 00:14:46 +0800

    webpack-externals插件引入及cdn引入

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,14 @@
 const {defineConfig} = require('@vue/cli-service')
+const nodeExternals = require('webpack-node-externals');
 module.exports = defineConfig({
     transpileDependencies: true,
     configureWebpack: config => {
         if (process.env.NODE_ENV === 'production') {
-            config.externals = {
-                'vue': 'Vue',
-                'element-ui': 'ELEMENT'
-            }
+            config.externals = [
+                nodeExternals()
+            ]
+            config.target = 'node'
+            config.externalsPresets = {node: true}
             config.entry = './src/main-prod.js'
         } else {
             config.entry = './src/main.js'
