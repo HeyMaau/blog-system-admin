8763ae03387747e1c51a2a82d00b4398631fe819
zhengwenbo <19802021891@139.com>
2023-05-28 11:10:55 +0800

    修改URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
-export const KEY_COOKIE_TOKEN = "manpok_blog_system_token"
 export const CODE_SUCCESS = 20000
 export const TYPE_ARTICLE_IMAGE = "1"
-export const URL_IMAGE = process.env.VUE_APP_SERVER_PATH + "/image/"
+export const URL_IMAGE = process.env.VUE_APP_IMAGE_URL
