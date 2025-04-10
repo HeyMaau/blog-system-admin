f7dde0439055a8f392876d9e793e3cc70170a34f
HeyMaau <841294180@163.com>
2025-04-10 21:04:40 +0800

    feat: 初步添加MD编辑器

---
+++ src/components/MdEditor.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="vditor"/>
+</template>
+
+<script setup lang="ts">
+import {ref, onMounted} from 'vue';
+import Vditor from 'vditor';
+import 'vditor/dist/index.css';
+
+const vditor = ref<Vditor | null>(null);
+
+onMounted(() => {
+  vditor.value = new Vditor('vditor', {
+    minHeight: 500,
+    placeholder: "请随便写吧",
+    counter: {
+      enable: true
+    }
+  });
+});
+
+</script>
+
+<style scoped></style>
--- src/components/pages/EditArticlePage.vue
+++ src/components/pages/EditArticlePage.vue
@@ -4,12 +4,12 @@
       <textarea id="input-area" placeholder="请输入标题" rows="1" ref="inputAreaRef" v-model="article.title"></textarea>
     </label>
     <div class="editor-save-button-container">
-      <RtEditor v-model="article.content"/>
+      <RtEditor v-model="article.content" v-show="false"/>
+      <MdEditor class="md-editor"/>
       <div class="save-button-container" ref="saveButtonContainerRef">
         <div class="save-button" @click="saveArticle">
           <svg ref="refreshIconRef" t="1686294507516" class="icon refresh-icon" viewBox="0 0 1024 1024" version="1.1"
-               xmlns="http://www.w3.org/2000/svg"
-               p-id="3374" width="64" height="64">
+               xmlns="http://www.w3.org/2000/svg" p-id="3374" width="64" height="64">
             <path
                 d="M929.8 353.9c-5.5-14.5-21.6-21.8-36.1-16.4-14.5 5.5-21.8 21.6-16.3 36.1 36.8 97.8 33.4 204-9.6 299.2-43.2 95.2-120.7 167.9-218.5 204.8-97.8 36.8-204 33.4-299.2-9.6-33.6-15.2-64.8-35.2-92.7-59.1 22.4 0.6 46.9-0.5 72.1-5 15.2-2.7 25.4-17.3 22.7-32.5-2.7-15.2-17.3-25.4-32.5-22.7-50.8 9-99.2 2.1-126.5-3.8-9.2-4.2-20.4-3.4-29 3.3-3.4 2.6-5.9 5.9-7.7 9.5-0.2 0.4-0.3 0.8-0.5 1.2-0.6 1.3-1.1 2.5-1.4 3.9-1.6 5.3-1.9 11-0.2 16.5 7.2 23.1 24.2 92.3 17.4 134.7-2.5 15.3 7.9 29.7 23.2 32.2 1.5 0.2 3 0.4 4.5 0.4 13.5 0 25.4-9.8 27.6-23.6 3.3-20.5 2.6-43.8 0.2-66.2 30.3 24.9 63.9 46.1 99.8 62.3 58.8 26.6 121.4 40 184.1 40 53.2 0 106.6-9.6 158-29C780.9 887.9 869.5 804.8 918.8 696c49.2-108.8 53.1-230.3 11-342.1zM155 350.5c88.9-196.5 321.1-284 517.5-195.1 32.8 14.8 62.6 33.7 89.6 56.2-22.4-0.6-46.8 0.5-72 5-15.2 2.7-25.4 17.2-22.7 32.5 2.7 15.3 17.3 25.4 32.5 22.7 54.5-9.6 106.3-1 132.3 5.1 0.4 0.1 0.8 0.1 1.2 0.1 1.3 0.3 2.6 0.4 4 0.5 0.4 0 0.8 0.1 1.1 0.1 0.3 0 0.6-0.1 0.9-0.1 1.1 0 2.3-0.2 3.4-0.3 0.7-0.1 1.4-0.2 2.1-0.4 1.1-0.3 2.2-0.6 3.2-1 0.7-0.3 1.4-0.5 2.1-0.8 1-0.4 1.9-1 2.8-1.6 0.7-0.4 1.5-0.9 2.2-1.4 0.2-0.2 0.5-0.3 0.7-0.5 0.6-0.5 1.1-1 1.6-1.6 0.5-0.4 1-0.8 1.4-1.2 0.1-0.2 0.2-0.4 0.4-0.5 1.5-1.7 2.8-3.5 3.8-5.4l0.3-0.6c0.9-1.7 1.5-3.6 2-5.5 0.2-0.6 0.3-1.3 0.4-1.9 0.3-1.6 0.4-3.3 0.5-5v-1.6c-0.1-2.2-0.5-4.4-1.1-6.5v-0.3c-7.2-23-24.3-92.2-17.4-134.7 2.5-15.3-7.9-29.7-23.2-32.2-15.2-2.5-29.7 7.9-32.2 23.2-3.3 20.5-2.6 43.8-0.2 66.3-29.3-23.7-61.6-43.7-96.7-59.6C471 2.6 205.6 102.7 104 327.3 54.7 436.2 50.8 557.7 93 669.5c4.2 11.2 14.9 18.2 26.2 18.2 3.3 0 6.6-0.6 9.9-1.8 14.5-5.5 21.8-21.6 16.3-36.1-36.8-97.9-33.4-204.2 9.6-299.3z"
                 fill="#4F4F4F" p-id="3375"></path>
@@ -28,14 +28,9 @@
         <el-row class="setting-item">
           <el-col :span="3" class="setting-item-title">文章封面</el-col>
           <el-col :span="8">
-            <el-upload
-                :headers="{'authorization': token}"
-                accept=".jpg,.jpeg,.png,.webp"
-                class="avatar-uploader"
-                :action="uploadImagePath"
-                :show-file-list="false"
-                :on-success="handleCoverSuccess"
-                :before-upload="beforeCoverUpload">
+            <el-upload :headers="{ 'authorization': token }" accept=".jpg,.jpeg,.png,.webp" class="avatar-uploader"
+                       :action="uploadImagePath" :show-file-list="false" :on-success="handleCoverSuccess"
+                       :before-upload="beforeCoverUpload">
               <img v-if="article.cover" :src="article.cover" class="avatar">
               <i v-else class="el-icon-plus avatar-uploader-icon"> 添加文章封面</i>
             </el-upload>
@@ -45,11 +40,7 @@
           <el-col :span="3" class="setting-item-title">文章分类</el-col>
           <el-col :span="8">
             <el-select v-model="article.categoryId" placeholder="请选择文章分类">
-              <el-option
-                  v-for="item in categoryList"
-                  :key="item.id"
-                  :label="item.name"
-                  :value="item.id">
+              <el-option v-for="item in categoryList" :key="item.id" :label="item.name" :value="item.id">
               </el-option>
             </el-select>
           </el-col>
@@ -57,23 +48,12 @@
         <el-row class="setting-item" type="flex" align="middle">
           <el-col :span="3" class="setting-item-title">文章标签</el-col>
           <el-col :span="21">
-            <el-tag
-                :key="tag"
-                v-for="tag in dynamicTags"
-                closable
-                :disable-transitions="false"
-                @close="handleClose(tag)">
+            <el-tag :key="tag" v-for="tag in dynamicTags" closable :disable-transitions="false"
+                    @close="handleClose(tag)">
               {{ tag }}
             </el-tag>
-            <el-input
-                class="input-new-tag"
-                v-if="inputVisible"
-                v-model="inputValue"
-                ref="saveTagInput"
-                size="small"
-                @keyup.enter="handleInputConfirm"
-                @blur="handleInputConfirm"
-            >
+            <el-input class="input-new-tag" v-if="inputVisible" v-model="inputValue" ref="saveTagInput" size="small"
+                      @keyup.enter="handleInputConfirm" @blur="handleInputConfirm">
             </el-input>
             <el-button v-else class="button-new-tag" size="small" @click="showInput">+ New Tag</el-button>
           </el-col>
@@ -81,15 +61,20 @@
       </div>
     </div>
     <div id="operation-container">
-      <el-button size="large" type="primary" @click="addArticle('2', false)" v-if="articleState !== '2'">发布文章</el-button>
-      <el-button size="large" type="info" @click="addArticle('1', false)" v-if="articleState !== '2'">保存文章</el-button>
-      <el-button size="large" type="primary" @click="updateArticle(false)" v-if="articleState === '2'">修改文章</el-button>
+      <el-button size="large" type="primary" @click="addArticle('2', false)"
+                 v-if="articleState !== '2'">发布文章
+      </el-button>
+      <el-button size="large" type="info" @click="addArticle('1', false)" v-if="articleState !== '2'">保存文章
+      </el-button>
+      <el-button size="large" type="primary" @click="updateArticle(false)" v-if="articleState === '2'">修改文章
+      </el-button>
     </div>
   </div>
 </template>
 
 <script>
 import RtEditor from "@/components/RtEditor.vue";
+import MdEditor from "../MdEditor.vue"
 import {getCategoriesApi} from "@/apis/category_api";
 import {CODE_SUCCESS, URL_UPLOAD_IMAGE} from "@/utils/constants";
 import {addArticleApi, getArticleApi, updateArticleApi} from "@/apis/article_api";
@@ -97,7 +82,8 @@ import {addArticleApi, getArticleApi, updateArticleApi} from "@/apis/article_api
 export default {
   name: "EditArticlePage",
   components: {
-    RtEditor
+    RtEditor,
+    MdEditor
   },
   data() {
     return {
@@ -259,11 +245,14 @@ export default {
 </script>
 
 <style scoped>
-
 #container {
   display: flex;
   align-items: center;
   flex-direction: column;
+}
+
+.md-editor {
+  width: 1000px !important;
 }
 
 .tinymce-editor {
@@ -289,7 +278,7 @@ export default {
   font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
 }
 
-:deep( .tox-tinymce) {
+:deep(.tox-tinymce) {
   border-radius: 5px;
 }
 
@@ -313,7 +302,7 @@ export default {
   margin-left: 20px;
 }
 
-:deep( .avatar-uploader .el-upload) {
+:deep(.avatar-uploader .el-upload) {
   border: 1px dashed #d9d9d9;
   border-radius: 6px;
   cursor: pointer;
@@ -321,7 +310,7 @@ export default {
   overflow: hidden;
 }
 
-:deep( .avatar-uploader .el-upload:hover) {
+:deep(.avatar-uploader .el-upload:hover) {
   border-color: #409EFF;
 }
 
@@ -407,9 +396,11 @@ export default {
   0% {
     transform: rotate(0deg)
   }
+
   50% {
     transform: rotate(180deg)
   }
+
   100% {
     transform: rotate(360deg)
   }
@@ -419,12 +410,13 @@ export default {
   0% {
     -webkit-transform: rotate(0deg)
   }
+
   50% {
     -webkit-transform: rotate(180deg)
   }
+
   100% {
     -webkit-transform: rotate(360deg)
   }
 }
-
 </style>
